58efaaae9117bd3a620fcd45f0a43e7f98bddd79
NamyaDev <namyadev@seznam.cz>
2024-08-17 00:03:14 +0200

    feat: refactor buttons

--- src/main/java/namya/project/ui/buttons/CommonButton.java
+++ src/main/java/namya/project/ui/buttons/CommonButton.java
@@ -4,8 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
-public abstract class CommonButtons extends JButton{
-    CommonButtons(ActionListener listener){
+public abstract class CommonButton extends JButton{
+    CommonButton(ActionListener listener){
         this.setBorder(BorderFactory.createLineBorder(Color.black,3));
         this.setFocusable(false);
         this.setFont(new Font("Arial",Font.BOLD,18));
